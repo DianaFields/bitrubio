7115cbeb07b6bd13283f2eb6f21d2bd46d653095
DianaFields <dian@iMac-de-Diana.local>
2017-03-29 00:28:58 -0600

    update

--- js/home.js
+++ js/home.js
@@ -5,18 +5,12 @@ function homeInit() {
 
       if(height  > 450) {
        $('#scrollParallax').css('background-attachment', 'fixed');
-       $('.screen-1').css('background-attachment', 'fixed');
+       $('.parallax').css('background-attachment', 'fixed');
       }
      
       else{
         $('#scrollParallax').css('background-attachment', 'inherit');
-        $('.screen-1').css('background-attachment', 'inherit');
-      };
-       if(height > 1200){
-         $('.screen-1').css('opacity', '0');
-      }
-      else{
-        $('.screen-1').css('opacity', '100');
+        $('.parallax').css('background-attachment', 'inherit');
       };
   });
      
