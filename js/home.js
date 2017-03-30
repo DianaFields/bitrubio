0afee6a316c215e606181c0660b81eda31ffd189
diana.campos <diana.campos@entrepids.com>
2017-03-30 16:31:25 -0600

    code

--- js/home.js
+++ js/home.js
@@ -10,46 +10,55 @@ function homeInit() {
   }
 });
 
+
+
 // parallax
 (function($){
-
             //Plugin activation
             $(window).enllax();
 
           })(jQuery);
+        }
+        var change = true;
+        var changea = true;
+        function changeScreen(){
+          $(window).scroll(function(){
+            var showMessageInterval = window.setInterval(showMessageIfNeeded, 500);
+           function showMessageIfNeeded() {
+            var scrollTop = $(window).scrollTop();
+            var firstScreen = $("#bloque-1").offset().top;
+            var secondScreen = $("#bloque-2").offset().top;
+            if (scrollTop > firstScreen) {
+              if (change) {
+               $('#screen-1').css('opacity','1')
+               window.clearInterval(showMessageInterval);
+               change = false;
+             }
+           }
+           if (scrollTop < firstScreen) {
+            if (change == false) {
+             $('#screen-1').css('opacity','0')
+             window.clearInterval(showMessageInterval);
+             change = true;
+           }
+         }
+    // 
+    if (scrollTop > secondScreen) {
+      if (changea) {
+       $('#screen-1').css('opacity','0')
+       $('#screen-2').css('opacity','1')
+       window.clearInterval(showMessageInterval);
+       changea = false;
+     }
+   }
+   if (scrollTop < secondScreen) {
+    if (changea == false) {
+     $('#screen-2').css('opacity','0')
+     window.clearInterval(showMessageInterval);
+     changea = true;
+   }
+ }
+
 }
-
-function changeScreen() {
-
-    var currentPosY = windowMidY + window.pageYOffset,
-      position,
-      position_top,
-      position_left = windowMidX,
-      margin_left = - phoneWidth / 2,
-      margin_top = - phoneHeight / 2,
-      mode;
-
-
-    transformPhone(position, position_top, position_left, margin_left, margin_top, mode);
-
-
-  function transformPhone(position, position_top, position_left, margin_left, margin_top, mode) {
-    for ( i = 0; i < 4; i++ ) {
-      if ( i == mode ) {
-        $( '#screen-' + i ).css({"opacity" : "1" });
-      }
-      else if (i != mode) {
-        $( '#screen-' + i ).css({ "opacity" : "0" });
-      }
-    }
-    $( '#floating-phone' ).css({ "display" : "block", "position" : position, "top" : position_top,
-      "left" : position_left, "margin-left" : margin_left, "margin-top" : margin_top });
-    $( '#floating-phone-scale-image' ).css({ "height" : phoneHeight, "width" : phoneWidth });
-    $('.phone-screen').css({"height": phoneHeight, "width": phoneWidth});
-  }
-
-}
-
-
-
-
+});
+        }
