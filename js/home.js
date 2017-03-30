99ac7377916be533d6950580e1f614d56b9a19cf
DianaFields <dian@iMac-de-Diana.local>
2017-03-30 00:02:30 -0600

    update

--- js/home.js
+++ js/home.js
@@ -3,30 +3,15 @@ function homeInit() {
  $(window).scroll(function() {
   var height = $(window).scrollTop();
   if (height  < 450){
+    $('.stop').css('background-attachment', 'inherit');
+     $('.screen-1').css('opacity', '0');
+  }
+  else if(height  > 450) {
     $('.stop').css('background-attachment', 'fixed');
-    $('.screen-1').css('opacity','0');
-    $('.screen-2').css('opacity','0');
   }
-  // else if(height  > 450) {
-  //   $('.stop').css('background-attachment', 'fixed');
-  // }
-  else ((height  < 600) && (height > 451)){
-    $('.screen-0').css('opacity','1');
-    $('.screen-1').css('opacity','0');
-  }
-  else ( (height  > 600) && (height < 800) ) {
-    $('.screen-0').css('opacity','0');
-    $('.screen-1').css('opacity','1');
-  }
-  
-  else ((height  > 801) && (height < 1200)) {
-    $('.screen-1').css('opacity','0');
-    $('.screen-2').css('opacity','1');
-  }
-  // else if (height  < 800) {
-  //   $('.screen-2').css('opacity','0');
-  // }
 });
+
+
 
 // parallax
 (function($){
@@ -35,6 +20,24 @@ function homeInit() {
             $(window).enllax();
 
           })(jQuery);
+}
+
+function homeInit2() {
+
+ $(window).scroll(function() {
+  var heightNew = $(window).scrollTop();
+  if (heightNew  > 450){
+    $('.screen-0').css('opacity', '0');
+    $('.screen-1').css('opacity', '1');
+  }
+  else(height  < 450) {
+     $('.screen-0').css('opacity', '1');
+    $('.screen-1').css('opacity', '0');
+  }
+});
+
+
+
 }
 
 
