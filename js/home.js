683a9df21d3ca8ff87ad3cf28b04f4d143730853
diana.campos <diana.campos@entrepids.com>
2017-04-03 17:45:39 -0500

    monday work

--- js/home.js
+++ js/home.js
@@ -1,7 +1,4 @@
 function homeInit() {
-
-
-
 // parallax
 (function($){
             //Plugin activation
@@ -24,6 +21,10 @@ function homeInit() {
               $('.stop').css('background-attachment', 'fixed');
               $('.parallax').css('background-attachment', 'fixed');
             }
+            if (height  > 2800){
+              $('.stop').css('background-attachment', 'inherit');
+              $('.parallax').css('background-attachment', 'inherit');
+            }
             // fixed
 
 
@@ -32,10 +33,10 @@ function homeInit() {
               var scrollTop = $(window).scrollTop();
               var firstScreen = $("#bloque-1").offset().top;
                var secondScreen = $("#bloque-2").offset().top;
-              if (scrollTop > firstScreen) {
+              if (scrollTop > 700) {
                 if (change) {
-                 $('#screen-1').css('opacity','1')
-                 // $('#screen-0').css('opacity','0')
+                  // $('#screen-0').css('opacity','0');
+                  $('#screen-1').css('opacity','1');
                  window.clearInterval(showMessageInterval);
                  change = false;
                }
